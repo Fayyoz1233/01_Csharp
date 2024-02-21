fa9fa0acfad23f4e395bd44cfb3303af9137fd86
Fayyoz <fayyoztoshov@gmail.com>
2024-02-22 02:38:57 +0500

    Квадр корень

--- Desktop/Csharp1/Task1/Program.cs
+++ Desktop/Csharp1/Task1/Program.cs
@@ -1,3 +1,10 @@
-﻿# ZZZ
-
-djnsdc
+﻿Console.WriteLine("num1: ");
+string var1 = Console.ReadLine();
+int var_num1 = int.Parse(var1);
+Console.WriteLine("num2: ");
+string var2 = Console.ReadLine();
+int var_num2 = int.Parse(var2);
+if (var_num1 == var_num2 * var_num2)
+    Console.Write("Yes");
+else    
+    Console.Write("No");

26ae872bc8842a5a29c183df8077ed802685d3b7
Fayyoz <fayyoztoshov@gmail.com>
2024-03-21 20:56:53 +0500

    L5T1.1

--- Desktop/01_Csharp/Lesson5/Task1/Program.cs
+++ Desktop/01_Csharp/Lesson5/Task1/Program.cs
@@ -1,9 +1,11 @@
 ﻿int Fact(int n)
 {
-    if(n == 1)
+    if(n == 1 || n == 0)
     {
+        Console.WriteLine($"Stop: {n}");
         return 1;
     }
+    Console.WriteLine(n);
     return n * Fact(n-1);
 }
 
